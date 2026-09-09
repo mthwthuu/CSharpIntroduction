b0ab298e787b036b26204cddd0da436fd40d44d6
cmttt <cmttt@CHUMINHTHU8A3F>
2026-09-09 17:11:48 -0500

    My demo week 3

---
+++ Week03/Demo/Minh.Week3Demo/Minh.Week3Demo.UI/MinhDemoW3.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Minh.Week3Demo.UI
+{
+    partial class MinhDemoW3
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnEnter = new Button();
+            SuspendLayout();
+            // 
+            // btnEnter
+            // 
+            btnEnter.Location = new Point(210, 115);
+            btnEnter.Name = "btnEnter";
+            btnEnter.Size = new Size(348, 200);
+            btnEnter.TabIndex = 0;
+            btnEnter.Text = "Enter";
+            btnEnter.UseVisualStyleBackColor = true;
+            btnEnter.Click += btnEnter_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnEnter);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnEnter;
+    }
+}
---
+++ Week03/Demo/Minh.Week3Demo/Minh.Week3Demo.UI/MinhDemoW3.cs
@@ -0,0 +1,15 @@
+namespace Minh.Week3Demo.UI
+{
+    public partial class MinhDemoW3 : Form
+    {
+        public MinhDemoW3()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Hello, World!");
+        }
+    }
+}
