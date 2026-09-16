0f270feb36beba51a02b765d4d7f1e653bab5ef3
cmttt <cmttt@CHUMINHTHU8A3F>
2026-09-15 22:30:14 -0500

    create a state capital assignment

---
+++ Week03/Assignments/MC.StateCapitals/MC.StateCapitals.UI/frmStateCaptitals.cs
@@ -0,0 +1,23 @@
+namespace MC.StateCapitals.UI
+{
+    public partial class frmStateCapitls : Form
+    {
+        public frmStateCapitls()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // frmStateCapitls
+            // 
+            ClientSize = new Size(940, 517);
+            Name = "frmStateCapitls";
+            Text = "State Capitals";
+            ResumeLayout(false);
+
+        }
+    }
+}
